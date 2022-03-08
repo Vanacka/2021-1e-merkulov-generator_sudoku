35d26b97f302ee5210b4844e08b6d1e4b7600ee6
Ivan Merkulov <ivan.merkulov@student.gyarab.cz>
2022-03-08 10:59:12 +0100

    prvni verze

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        int[][] pole = new int[9][9];
+
+        for (int i = 0; i < pole.length; i++) {
+            for (int j = 0; j < pole.length; j++) {
+                pole[i][j] = r.nextInt(9);
+                pole[i][j] = pole[i][j] + 1;
+                System.out.print(pole[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(1+2+3+4+5+6+7+8+9);
+    }
+}
