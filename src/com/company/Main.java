9e25119964062cba708b98abacd5295b79b6dbd3
Ivan Merkulov <ivan.merkulov@student.gyarab.cz>
2022-03-08 11:00:15 +0100

    mmmmmmm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,6 +16,7 @@ public class Main {
             }
             System.out.println();
         }
-        System.out.println(1+2+3+4+5+6+7+8+9);
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+        System.out.println("Jses curak");
     }
 }
